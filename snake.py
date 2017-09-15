63fee5d33d3d383015f024f0ceb58f553b717adf
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-15 13:26:19 +0700

    create model & add method modelsprite

--- snake.py
+++ snake.py
@@ -2,7 +2,21 @@ import arcade
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
  
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SnakeWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
