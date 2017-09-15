4d600ebf16ff247b0d04de04deffdc326ad80af6
Arniwatt Chonkiattipoom <arniwatt.c@ku.th>
2017-09-15 14:02:27 +0700

    model

--- models.py
+++ models.py
@@ -1,21 +1,31 @@
+import arcade.key
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+DIR_OFFSET = { DIR_UP: (0,1),
+               DIR_RIGHT: (1,0),
+               DIR_DOWN: (0,-1),
+               DIR_LEFT: (-1,0) }
 class Snake:
     MOVE_WAIT = 0.2
+    BLOCK_SIZE = 16
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
         self.wait_time = 0
+        self.direction = DIR_RIGHT
  
     def update(self, delta):
         self.wait_time += delta
         if self.wait_time < Snake.MOVE_WAIT:
             return
- 
         if self.x > self.world.width:
             self.x = 0
-        self.x += 16
+        self.x += DIR_OFFSET[self.direction][0]*Snake.BLOCK_SIZE
+        self.y += DIR_OFFSET[self.direction][1]*Snake.BLOCK_SIZE
         self.wait_time = 0
- 
  
 class World:
     def __init__(self, width, height):
